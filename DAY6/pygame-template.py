89b6c866b9b90e06fc83276c1df35a17215887d9
Pujan-DEV <Pujanneupaneop0907@gmail.com>
2026-01-03 23:30:52 +0545

    Pygame

--- DAY6/pygame-template.py
+++ DAY6/pygame-template.py
@@ -6,14 +6,28 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Pygame Template")
 clock = pygame.time.Clock()
 WHITE = (255, 255, 255)
-
+BLUE = (0, 0, 255)
+player_x, player_y = 400, 300
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     screen.fill(WHITE)
+    # simulate keyboard input
+    if pygame.key.get_pressed()[pygame.K_LEFT]:
+        player_x -= 1
+    if pygame.key.get_pressed()[pygame.K_RIGHT]:
+        player_x += 1
+    if pygame.key.get_pressed()[pygame.K_UP]:
+        player_y -= 1
+    if pygame.key.get_pressed()[pygame.K_DOWN]:
+        player_y += 1
+    # pygames.draw.rect(screen, BLUE, (x,y,width,height))
+
+    pygame.draw.rect(screen, BLUE, (player_x, player_y, 100, 100))
+    pygame.display.update()
+    # screen.fill(WHITE)
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-class Users:
-    role = "Admin"
-    def __init__(self,name):
-        self.name = name
-    def __str__(self):
-        return f"User Name is {self.name} and Role is {self.role}"
-    def __repr__(self):
-        return f"Users('{self.name}')"
-
-u1 = Users("Alice")
-# print(u1)
-# print(u1.role)
-# print(u1.name)
-print(repr(u1))
+import sys
+print(sys.argv)
